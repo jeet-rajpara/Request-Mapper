e08c6f3c3830d588bdcf1921fd133deac2953f03
jeet-rajpara <jeetrajpara941@gmail.com>
2025-06-06 13:43:03 +0530

    fix error

--- backend/api/repository/request_mapper_repository.go
+++ backend/api/repository/request_mapper_repository.go
@@ -16,23 +16,22 @@ func NewRequestMapperRepository() RequestMapperRepository {
 
 // getValueFromInputJSON retrieves a value from the input JSON using a dot-notation path
 func getValueFromInputJSON(inputJSON map[string]any, requestMapFieldValueString string) (any, error) {
-
 	keys := strings.Split(requestMapFieldValueString, ".")
+	if len(keys) == 1 {
+		return requestMapFieldValueString, nil
+	}
 
 	current := inputJSON
 
 	for i, key := range keys {
 		if i == len(keys)-1 {
 			value, exists := current[key]
-			if !exists {
-				return requestMapFieldValueString, nil
-			}
 
-			if value == nil {
-				return "", nil
+			if value == nil && !exists {
+				return nil, nil
+			} else {
+				return value, nil
 			}
-
-			return value, nil
 		}
 
 		next, ok := current[key].(map[string]any)
